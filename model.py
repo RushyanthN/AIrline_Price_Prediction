d957cd7a40fcf5b5db2ce4443ef5cb28d219c29e
RushyanthN <156248625+RushyanthN@users.noreply.github.com>
2024-06-22 20:27:20 -0400

    Update model.py

--- model.py
+++ model.py
@@ -80,7 +80,7 @@ def train_xgboost_model(df):
     X = df.drop(columns='log_price', axis=1)
     y = df['log_price']
 
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.01, random_state=42)
 
     # Convert object data types to float
     for col in X_train.columns:
